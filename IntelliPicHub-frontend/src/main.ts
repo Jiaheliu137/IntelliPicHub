828e9cd0252711eefafe905c1021eaaad751591a
jiaheliu137 <jiahe.liu@qq.com>
2025-05-26 09:03:34 +0900

    Security Update

--- IntelliPicHub-frontend/src/main.ts
+++ IntelliPicHub-frontend/src/main.ts
@@ -12,21 +12,12 @@ import VueCropper from 'vue-cropper';
 import 'vue-cropper/dist/index.css'
 import '@/access/index.ts';
 
-// 引入禁用开发者工具功能
-import { initDisableDevTools } from '@/utils/disableDevTools';
-
-
-
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(Antd)
 app.use(VueCropper)
-
-// 初始化禁用开发者工具功能
-initDisableDevTools()
 
 app.mount('#app')
 
